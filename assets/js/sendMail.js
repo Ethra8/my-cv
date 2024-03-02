e0d75234154ebbf87541e72ad9c1d91fd807da88
EdnaTMunill <80659091+Ethra8@users.noreply.github.com>
2024-03-02 23:02:48 +0100

    fix vars

--- assets/js/sendMail.js
+++ assets/js/sendMail.js
@@ -1,9 +1,9 @@
 
 function sendMail(contactForm) {
     emailjs.send("service_ybrj39d", "template_y1hw4k6", {
-        "user_name": document.getElementById("name").value(),
-        "user_email": document.getElementById("email").value(),
-        "project_summary": document.getElementById("projectsummary").value()
+        "user_name": contactForm.name.value,
+        "user_email": contactForm.email.value,
+        "project_summary": contactForm.projectsummary.value
     })
     .then(
         function(response){
