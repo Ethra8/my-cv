a8260ee3dc42303a7805f337a071a9c365c5d2d1
EdnaTMunill <80659091+Ethra8@users.noreply.github.com>
2024-03-02 19:56:40 +0100

    update vars

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,9 +1,9 @@
 
 function sendEmail(contactForm) {
     emailjs.send("service_ybrj39d", "template_y1hw4k6", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.email.value,
-        "project_request": contactForm.projectsummary.value
+        "from_name": contactForm.user_name.value,
+        "from_email": contactForm.user_email.value,
+        "project_request": contactForm.project_summary.value
     })
     .then(
         function(response){
