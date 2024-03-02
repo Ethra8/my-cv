e25e0df4328b56e52ea86735088842ea637f0cdf
EdnaTMunill <80659091+Ethra8@users.noreply.github.com>
2024-03-02 20:36:59 +0100

    simplify var names

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,9 +1,9 @@
 
 function sendEmail(contactForm) {
     emailjs.send("service_ybrj39d", "template_y1hw4k6", {
-        "user_name": contactForm.user_name.value,
-        "user_email": contactForm.user_email.value,
-        "project_summary": contactForm.project_summary.value
+        "user_name": contactForm.name.value,
+        "user_email": contactForm.email.value,
+        "project_summary": contactForm.projectsummary.value
     })
     .then(
         function(response){
